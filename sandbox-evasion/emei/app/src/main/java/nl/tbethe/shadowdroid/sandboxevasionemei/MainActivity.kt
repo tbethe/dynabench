8ba0c95fd409ed01e98805e614e0e96773ddba9d
Timme Bethe <timme.bethe@gmail.com>
2021-03-26 11:18:10 +0100

    Initial commit of SandboxEvasionEMEI. Tries to detect sandbox by reading the EMEI.

---
+++ sandbox-evasion/emei/app/src/main/java/nl/tbethe/shadowdroid/sandboxevasionemei/MainActivity.kt
@@ -0,0 +1,11 @@
+package nl.tbethe.shadowdroid.sandboxevasionemei
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
